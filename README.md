4df7f8acab776bbe25f32a57f89214ba98a0da8d
Adem Mohammed <45444428+abulhuman@users.noreply.github.com>
2022-07-12 11:33:41 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Momento Game
+
+*A simple momento memory game from the fireship.io React Course*
+
+Hosted live at: [TLOH](https://momento-game.web.app/) (This Link Over Here. lol 🤣)  
+Screenshot: [TBA]()
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+function Card({ image, selected, onClick }) {
+  return (
+    <div className='card'>
+      <div className={selected && 'selected'}>
+        <img src={image} alt='' className='card-face' />
+        <img
+          src={'/assets/fireship.png'}
+          alt=''
+          className='card-back'
+          onClick={onClick}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Card
